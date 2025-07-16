86ee7da1095086f3a7f893168c8df8afe765b773
kagura989 <k36955692@gmail.com>
2025-07-16 16:08:05 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,189 +1,57 @@
-# IAM role for CodePipeline
-resource "aws_iam_role" "codepipeline_role" {
-  name = "codepipeline-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "codepipeline.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+provider "aws" { region = var.region }
 
-resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
-  role       = aws_iam_role.codepipeline_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
-}
+resource "aws_s3_bucket" "artifact_bucket" { bucket = "${var.bucket_name}-${random_id.bucket_id.hex}" force_destroy = true }
 
-# IAM role for CodeBuild
-resource "aws_iam_role" "codebuild_role" {
-  name = "codebuild-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "codebuild.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+resource "random_id" "bucket_id" { byte_length = 4 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_policy" {
-  role       = aws_iam_role.codebuild_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
-}
+resource "aws_iam_role" "codepipeline_role" { name = "codepipeline-role"
 
-# IAM role for CodeDeploy
-resource "aws_iam_role" "codedeploy_role" {
-  name = "codedeploy-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "codedeploy.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+assume_role_policy = jsonencode({ Version = "2012-10-17" Statement = [ { Action = "sts:AssumeRole" Effect = "Allow" Principal = { Service = "codepipeline.amazonaws.com" } } ] }) }
 
-resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
-}
+resource "aws_iam_role_policy_attachment" "codepipeline_policy" { role       = aws_iam_role.codepipeline_role.name policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess" }
 
-# S3 bucket for artifacts
-resource "aws_s3_bucket" "artifact_bucket" {
-  bucket = "${var.github_owner}-devops-lab-pipeline"
-  force_destroy = true
-}
+resource "aws_iam_role" "codebuild_role" { name = "codebuild-role"
 
-# CodeBuild Project
-resource "aws_codebuild_project" "build_project" {
-  name          = "codebuild-project"
-  service_role  = aws_iam_role.codebuild_role.arn
+assume_role_policy = jsonencode({ Version = "2012-10-17" Statement = [ { Action = "sts:AssumeRole" Effect = "Allow" Principal = { Service = "codebuild.amazonaws.com" } } ] }) }
 
-  source {
-    type            = "GITHUB"
-    location        = "https://github.com/${var.github_owner}/${var.github_repo}.git"
-    git_clone_depth = 1
+resource "aws_iam_role_policy_attachment" "codebuild_policy" { role       = aws_iam_role.codebuild_role.name policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess" }
+
+resource "aws_codebuild_project" "build_project" { name          = "codebuild-project" description   = "Build project for CodePipeline" service_role  = aws_iam_role.codebuild_role.arn
+
+artifacts { type = "CODEPIPELINE" }
+
+environment { compute_type                = "BUILD_GENERAL1_SMALL" image                       = "aws/codebuild/standard:5.0" type                        = "LINUX_CONTAINER"
+
+environment_variables = [
+  {
+    name  = "EC2_IP"
+    value = var.ec2_ip
+  },
+  {
+    name  = "PRIVATE_KEY"
+    value = var.private_key
+    type  = "PLAINTEXT"
   }
+]
 
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-environment {
-  compute_type                = "BUILD_GENERAL1_SMALL"
-  image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-  type                        = "LINUX_CONTAINER"
-  environment_variable {
-    name  = "INSTANCE_IP"
-    value = "your-ec2-public-ip"
-  }
-}
-  
 }
 
-# CodeDeploy App
-resource "aws_codedeploy_app" "code_app" {
-  name = "code-app"
-  compute_platform = "Server"
-}
+source { type = "CODEPIPELINE" } }
 
-# CodeDeploy Deployment Group
-resource "aws_codedeploy_deployment_group" "deploy_group" {
-  app_name              = aws_codedeploy_app.code_app.name
-  deployment_group_name = "code-deploy-group"
-  service_role_arn      = aws_iam_role.codedeploy_role.arn
+resource "aws_codepipeline" "codepipeline" { name     = "devops-lab-pipeline" role_arn = aws_iam_role.codepipeline_role.arn
 
-  deployment_style {
-    deployment_type = "IN_PLACE"
-    deployment_option = "WITHOUT_TRAFFIC_CONTROL"
-  }
+artifact_store { location = aws_s3_bucket.artifact_bucket.bucket type     = "S3" }
 
-  ec2_tag_set {
-    ec2_tag_filter {
-      key   = "CodeDeploy"
-      type  = "KEY_AND_VALUE"
-      value = "EC2Instance"
-    }
-  }
+stage { name = "Source"
 
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
-  }
-}
+action {
+  name             = "Source"
+  category         = "Source"
+  owner            = "ThirdParty"
+  provider         = "GitHub"
+  version          = "1"
+  output_artifacts = ["source_output"]
 
-# CodePipeline
-resource "aws_codepipeline" "codepipeline" {
-  name     = "codepipeline-project"
-  role_arn = aws_iam_role.codepipeline_role.arn
-  artifact_store {
-    location = aws_s3_bucket.artifact_bucket.bucket
-    type     = "S3"
-  }
-
-  stage {
-    name = "Source"
-
-    action {
-      name             = "SourceAction"
-      category         = "Source"
-      owner            = "ThirdParty"
-      provider         = "GitHub"
-      version          = "1"
-      output_artifacts = ["source_output"]
-
-      configuration = {
-        Owner      = var.github_owner
-        Repo       = var.github_repo
-        Branch     = "main"
-        OAuthToken = var.github_token
-      }
-    }
-  }
-
-  stage {
-    name = "Build"
-
-    action {
-      name             = "BuildAction"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
-      version          = "1"
-
-      configuration = {
-        ProjectName = aws_codebuild_project.build_project.name
-      }
-    }
-  }
-
-  stage {
-    name = "Deploy"
-
-    action {
-      name            = "DeployAction"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "CodeDeploy"
-      input_artifacts = ["build_output"]
-      version         = "1"
-
-      configuration = {
-        ApplicationName     = aws_codedeploy_app.code_app.name
-        DeploymentGroupName = aws_codedeploy_deployment_group.deploy_group.deployment_group_name
-      }
-    }
-  }
-}
+  configuration = {
+    Owner      = var.github_owner
+    Repo       = var.github_repo
